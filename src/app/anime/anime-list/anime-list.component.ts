4a3326b366d45b8b6e380328c0922c5f336ead7c
Andres Molano <a.molanob@uniandes.edu.co>
2024-11-22 11:53:43 -0500

    mejore de vista y rating y episodio

--- src/app/anime/anime-list/anime-list.component.ts
+++ src/app/anime/anime-list/anime-list.component.ts
@@ -8,16 +8,39 @@ import { AnimeService } from '../anime.service';
   styleUrls: ['./anime-list.component.css']
 })
 export class AnimeListComponent implements OnInit {
-
   selectedBAnime!: Anime;
   selected = false;
   animes: Array<Anime> = [];
+  totalEpisodios = 0; // Almacena el total de episodios
+  ratingPromedio = 0; // Almacena el promedio de ratings
+
   constructor(private animeService: AnimeService) { }
 
   getAnimes(): void {
     this.animeService.getAnimes().subscribe((animes) => {
       this.animes = animes;
+      this.calcularEpyRat(); // Llamar al cálculo tras cargar los animes
     });
+  }
+
+  calcularEpyRat(): void {
+    let totalEpisodiosTemp = 0;
+    let totalRatingTemp = 0;
+
+    this.animes.forEach((anime) => {
+      totalEpisodiosTemp += anime.episode;
+
+      // Convertimos el rating a número, manejando posibles errores de conversión
+      const ratingNumerico = parseFloat(anime.Rating);
+      if (!isNaN(ratingNumerico)) {
+        totalRatingTemp += ratingNumerico;
+      }
+    });
+
+    this.totalEpisodios = totalEpisodiosTemp;
+    this.ratingPromedio = this.animes.length > 0 
+      ? totalRatingTemp / this.animes.length 
+      : 0; // Manejar división por cero
   }
 
   onSelected(anime: Anime): void {
@@ -28,5 +51,4 @@ export class AnimeListComponent implements OnInit {
   ngOnInit() {
     this.getAnimes();
   }
-
 }
